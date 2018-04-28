570572812104134ef603e23e39040abc7bdc15aa
John Dowling <john.patrick.dowling@gmail.com>
2018-04-28 14:24:49 -0700

    Update index.js

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ function RTL433Platform(log, config, api) {
   this.log = log;
   this.config = config;
   this.accessories = [];
+  this.cmdFlags = [];
   
   this.protocols = [ 73 ];
   this.device = config['device'] || "RTL2838";
@@ -33,11 +34,10 @@ function RTL433Platform(log, config, api) {
     this.protocols = config.protocols;
   }
   
-  this.cmdFlags = ['-F', 'json', '-d', this.device];
+  Array.prototype.push.apply(this.cmdFlags, ['-F', 'json', '-d', this.device]);
   this.protocols.forEach(function(protocol) {
     log("Adding protocol ", protocol);
-    cmdFlags.push('-R');
-    cmdFlags.push(protocol);
+    Array.prototype.push.apply(this.cmdFlags, ['-R', protocol]);
   });
   
   if (typeof(config.aliases) !== "undefined" && config.aliases !== null) {
